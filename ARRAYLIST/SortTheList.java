e95a2bf2dfb976299b964d7fde3a8ebd5f73f8ce
digitalvasanth <digitalvasanth01@gmail.com>
2024-03-15 12:25:27 +0530

    commit done

---
+++ ARRAYLIST/SortTheList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortTheList {
+    public static void main(String [] args){
+        ArrayList<Integer>list=new ArrayList<>();
+   
+
+    list.add(8);
+    list.add(1);
+    list.add(2);
+    list.add(2);
+
+    System.out.println("Before Sort");
+    System.out.println(list);
+
+    Collections.sort(list);
+    System.out.println("After sort");
+    System.out.println(list);
+
+    }
+    
+}
