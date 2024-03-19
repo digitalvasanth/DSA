3de922e7749ed961d1bd4a86f3d9f80cb1a794f2
digitalvasanth <digitalvasanth01@gmail.com>
2024-03-19 21:46:35 +0530

    commit done

--- 1. ARRAYS/Maxsubarraysumprefix.java
+++ 1. ARRAYS/Maxsubarraysumprefix.java
@@ -1,16 +1,20 @@
 public class Maxsubarraysumprefix {
-    public static int sum(int arr[]){
+    public static int sum(int arr[])
+    {
         int currsum=0;
         int maxsum=Integer.MIN_VALUE;
         int prefix[]=new int[arr.length];
 
         prefix[0]=arr[0];
-        for(int i=1;i<arr.length;i++){
+        for(int i=1;i<arr.length;i++)
+        {
             prefix[i]=prefix[i-1]+arr[i];
         }
 
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=0;j<arr.length;j++)
+            {
                 currsum=i==0?prefix[j]:prefix[j]-prefix[i-1];
             }
             maxsum=Math.max(currsum,maxsum);
---
+++ 5. STRINGS/FindLargestString.java
@@ -0,0 +1,16 @@
+public class FindLargestString 
+{
+    public static void main(String[]args)
+    {
+        String arr[]={"Mango","Apple","Orange"};
+        String largest=arr[0];
+
+        for(int i=1;i<arr.length;i++)
+        {
+            if(largest.compareTo(arr[i])>0)
+            largest=arr[i];
+        }
+        System.out.println("Largest string is "+ largest);
+        
+    }
+}
