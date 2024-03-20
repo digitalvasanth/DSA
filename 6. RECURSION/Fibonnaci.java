9fe0e87434ea4cb8cf225c7492e4adcfae3f6661
digitalvasanth <digitalvasanth01@gmail.com>
2024-03-20 15:23:26 +0530

    commit done

---
+++ 6. RECURSION/Fibonnaci.java
@@ -0,0 +1,17 @@
+class Fibbonacci{
+    public static int fib(int x){
+        if(x==0||x==1)
+        return 1;
+
+        int fib1=fib(x-1);
+        int fib2=fib(x-2);
+
+        return fib1+fib2;
+    }
+    public static void main(String [] args)
+    {
+        // int n=5;
+
+        System.out.println(fib(23));
+    }
+}
