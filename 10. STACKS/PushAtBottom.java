e43d72c29765c9b291b998b1f1ec41742be9f877
digitalvasanth <digitalvasanth01@gmail.com>
2024-04-12 11:09:32 +0530

    commit done

---
+++ 10. STACKS/PushAtBottom.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class PushAtBottom {
+    public static void pushatbottom(Stack<Integer>s, int data)
+    {
+        if(s.isEmpty())
+        {
+            s.push(data);
+            return;
+        }
+        int top=s.pop();
+        pushatbottom(s, data);
+        s.push(top);
+    }
+    public static void main(String [] args)
+    {
+        Stack<Integer> s=new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);   
+
+
+        pushatbottom(s,100);
+        while(!s.isEmpty())
+        {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+     }
+    
+}
