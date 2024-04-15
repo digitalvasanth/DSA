21153cce6aefe528ce9d9f0ed8fa18676bf3a9f4
digitalvasanth <digitalvasanth01@gmail.com>
2024-04-15 14:50:22 +0530

    commit

---
+++ 11.QUEUES/CircularQueueWithArrays.java
@@ -0,0 +1,77 @@
+public class CircularQueueWithArrays {
+
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear=-1;
+        static int front =-1;
+
+        Queue(int n)
+        {
+            arr=new int[n];
+            this.size=n;
+        }
+        public static boolean isEmpty(){
+            return rear==-1&&front==-1;
+        }
+
+        public static boolean isFull()
+        {
+            return (rear+1)%size==front;
+        }
+
+        //enque
+        public static void add(int data)
+        {
+            if(isFull())
+            return;
+
+            if(front==-1)
+            front =0;
+            rear=(rear+1)%size;
+            arr[rear]=data;
+
+        }
+
+
+        //dequue
+        public static int remove()
+        {
+            if(isEmpty())
+            return -1;
+
+            int result=arr[0];
+            if(front==rear)
+            front=rear=-1;
+            else
+            front=(front+1)%size;
+
+            return result;
+        }
+
+
+        //peek
+        public static int peek(){
+            if(isEmpty())
+            return -1;
+
+            return arr[front];
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        Queue q=new Queue(5);
+        q.add(4);
+        q.add(3);
+        q.add(2);
+        q.add(1);
+    
+        while(!q.isEmpty())
+        {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+    
+}
